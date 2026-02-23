bcbb17b87f30c1266ac80bb0af109242e0950b04
rajveersinhjadeja126798-cmyk <rajveersinh.jadeja126798@marwadiuniversity.ac.in>
2026-02-23 10:34:09 +0530

    Add files via upload

---
+++ pro13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pro13
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            a = new int[10];
+
+            Console.WriteLine("enter 10 value");
+
+            int i;
+            for(i = 0; i<10; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (i = a.length - 1; i > 0; i--)
+            {
+                Console.WriteLine(a[i]);
+            }
+
+        }
+    }
+}
---
+++ program14/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace program14
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            String name;
+
+            Console.WriteLine("enter name");
+            name = Console.ReadLine();
+
+            string revaname = " ";
+            int i;
+            for (i = name.Length - 1; i >= 0; i--)
+            {
+
+                revaname += name[i];
+            
+            }
+            if (name == revaname)
+            {
+                Console.WriteLine("palidron");
+            }
+            else
+            {
+                Console.WriteLine(("not palidron");
+                
+            }
+
+        }
+    }
+}
